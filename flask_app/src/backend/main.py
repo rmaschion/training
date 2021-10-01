e623e8e9bd29ca39512ac1bcc3e504d24ade4edc
renato <rmaschion@gmail.com>
2021-10-01 16:37:20 -0700

    adding flash to heroku

--- flask_app/src/backend/main.py
+++ flask_app/src/backend/main.py
@@ -1,10 +1,12 @@
 import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.curdir, '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.curdir, '../../../webview')))
 sys.path.append(os.path.abspath(os.path.curdir))
 
 # print(os.path.abspath(os.path.curdir))
 import logging
+print("BEFORE IMPORT")
 import webview
+print("AFTER IMPORT")
 
 from contextlib import redirect_stdout
 from io import StringIO
