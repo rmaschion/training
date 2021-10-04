43d65ea701ee6469007ea71f9a0fb923f3438821
renato <rmaschion@gmail.com>
2021-10-04 13:44:10 -0700

    adding flash to heroku

--- app/main.py
+++ app/main.py
@@ -8,31 +8,31 @@ from server import server
 
 app = Flask(__name__)
 
-logger = logging.getLogger(__name__)
-stream = StringIO()
-with redirect_stdout(stream):
-    window = webview.create_window('My first pywebview application', server)
-    print('WINDOW IS CREATED')
-    webview.start(debug=True)
+# logger = logging.getLogger(__name__)
+# stream = StringIO()
+# with redirect_stdout(stream):
+#     window = webview.create_window('My first pywebview application', server)
+#     print('WINDOW IS CREATED')
+#     webview.start(debug=True)
+#
+# def initialize():
+#     # perform heavy stuff here
+#     return True
+#
+#
+# def do_stuff():
+#     # do whatever you need to do
+#     response = "This is response from Python backend"
+#     return response
 
-def initialize():
-    # perform heavy stuff here
-    return True
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
 
-
-def do_stuff():
-    # do whatever you need to do
-    response = "This is response from Python backend"
-    return response
-
-# @app.route("/")
-# def home_view():
-#     return "<h1>Welcome to Geeks for Geeks</h1>"
-
-# if __name__ == '__main__':
-#     pass
-    # stream = StringIO()
-    # with redirect_stdout(stream):
-    #     window = webview.create_window('My first pywebview application', server)
-    #     print('WINDOW IS CREATED')
-    #     webview.start(debug=True)
+if __name__ == '__main__':
+    pass
+    stream = StringIO()
+    with redirect_stdout(stream):
+        window = webview.create_window('My first pywebview application', server)
+        print('WINDOW IS CREATED')
+        webview.start(debug=True)
