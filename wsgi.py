03fc16a3c77b861e06d76f2fcdf2ce941fdfce40
renato <rmaschion@gmail.com>
2021-10-04 16:40:18 -0700

    returning changes

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,26 @@
+import webview
+from flask import Flask
+
 print("************BEFORE IMPORT********")
 from app.main import app
 print("************after IMPORT********")
+print("************BEFORE FLASK********")
+app = Flask(__name__)
+print("************after FLASK********")
+
+
+from contextlib import redirect_stdout
+from io import StringIO
+from server import server
+
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    stream = StringIO()
+    with redirect_stdout(stream):
+        window = webview.create_window('My first pywebview application', server)
+        print('WINDOW IS CREATED')
+        webview.start(debug=True)
