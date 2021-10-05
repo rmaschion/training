2d27b24e3d8c0bfd7949cc38ee29f7313b7b3ba1
renato <rmaschion@gmail.com>
2021-10-04 17:08:45 -0700

    adding flash to heroku

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 import webview
 from flask import Flask
 
+from app.server import server
+
+from contextlib import redirect_stdout
+from io import StringIO
+
 # print("************BEFORE IMPORT********")
 # from app.main import app
 # print("************after IMPORT********")
@@ -14,7 +19,13 @@ from io import StringIO
 
 @app.route("/")
 def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+    # logger = logging.getLogger(__name__)
+    stream = StringIO()
+    with redirect_stdout(stream):
+        window = webview.create_window('My first pywebview application', server)
+        print('WINDOW IS CREATED')
+        webview.start(debug=True)
+    # return "<h1>Welcome to Geeks for Geeks</h1>"
 
 if __name__ == "__main__":
     app.run()
